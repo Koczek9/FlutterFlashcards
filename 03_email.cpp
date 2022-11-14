b71fd6ed53970e962aae5f4e63dd25b26dc6023e
Katarzyna_Koczwara <katarzyna.b.koczwara@gmail.com>
2022-11-14 12:05:34 +0100

    created email programm

---
+++ 03_email.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    std::string name;
+    int space;
+
+    std::cout << "What's your full name? " << std::endl;
+    std::getline (std::cin, name);
+
+    space = name.find(' ');
+
+    //std::cout<< space << std::endl;
+
+    if(name.empty()){
+        std::cout << "Please enter valid name" << std::endl;
+    }
+    else if(space==-1){
+        std::cout << "Your Id is " << name.append("@gmail.com");
+    } 
+    else{
+        //name.erase(space,1);
+        name.replace(space,1,".");
+        std::cout << "Your Id is " << name.append("@gmail.com");
+    }
+
+    //name.erase(name.find(' '),1);
+
+
+    return 0;
+}
