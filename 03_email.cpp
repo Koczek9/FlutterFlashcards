8a116ec16f0ee8bcf9fe5579733666904f5854f9
Katarzyna_Koczwara <katarzyna.b.koczwara@gmail.com>
2022-11-15 14:53:00 +0100

    added recursive function replacing white spaces by dots

--- 03_email.cpp
+++ 03_email.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
+#include <algorithm>
+
+std::string dot(std::string name, int space);
 
 int main() {
     std::string name;
     int space;
 
-    std::cout << "What's your full name? " << std::endl;
-    std::getline (std::cin, name);
-
-    space = name.find(' ');
-
-    //std::cout<< space << std::endl;
-
     while(name.empty()){
-        std::cout << "Please enter valid name" << std::endl;
+        std::cout << "Please enter full name" << std::endl;
         std::getline (std::cin, name);
     }
 
-    if(space==-1){
-        std::cout << "Your Id is " << name.append("@gmail.com");
-    } 
-    else{
-        //name.erase(space,1);
-        name.replace(space,1,".");
-        std::cout << "Your Id is " << name.append("@gmail.com");
-    }
-
-    //name.erase(name.find(' '),1);
-
+    std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+    
+    name = dot(name, space);
+    std::cout << "Your Id is " << name.append("@gmail.com");
 
     return 0;
+}
+
+std::string dot(std::string name, int space){
+    space = name.find(' ');
+
+    if(space==-1){
+        return name;
+    }
+    else{
+        name.replace(space,1,".");
+        return dot(name, space);
+    }
 }
