b9452875a46956163d304b43cf2ce9f59bfef53c
Katarzyna_Koczwara <katarzyna.b.koczwara@gmail.com>
2022-11-22 15:26:48 +0100

    First step to more object orientaded geometry.

--- 07_geometry_classes.cpp
+++ 07_geometry_classes.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
 #include <cmath>
+#include <list>
+#include <vector>
 
 class AbstractFigure {
     public:
@@ -82,9 +84,20 @@ class Triangle : public AbstractFigure
     }
 };
 
+class FigureHelper
+{
+    public:
+    std::string name;
+    std::vector<std::string> inputNames;
+    FigureHelper(std::string n, std::vector<std::string> names) : name(n), inputNames(names) 
+    {
+
+    }
+};
+
 int main() 
 {
-    Circle circle1 = Circle(4);
+    /* Circle circle1 = Circle(4);
     Circle circle2 = Circle(5);
     Square square1 = Square(6);
     Square square2 = Square(2);
@@ -96,7 +109,63 @@ int main()
     square1.print_circumference();
     square1.print_area();
 
-    triangle1.print_area();
+    triangle1.print_area(); */
 
+    std::vector<FigureHelper> possibleFigures;
+    possibleFigures.emplace_back(FigureHelper("square", {"edge"}));
+    possibleFigures.emplace_back(FigureHelper("circle", {"radius"}));
+    possibleFigures.emplace_back(FigureHelper("triangle", {"A", "B", "C"}));
+
+    std::unique_ptr<AbstractFigure> myFigure;
+
+    std::cout << "Please choose one of the following figures to count circumference and area:" << std::endl;
+    
+    for (auto figure : possibleFigures)
+    {
+        std::cout << figure.name << std::endl;
+    }
+
+    std::string figureName;
+    std::cin >> figureName;
+
+        if(figureName == "square")
+    {
+        std::cout << "Enter length of " << possibleFigures[0].inputNames[0] << std::endl;
+        double edge;
+        std::cin >> edge;
+        myFigure = std::make_unique<Square>(edge);
+    }
+    else if(figureName == "circle")
+    {
+        std::cout << "Enter length of " << possibleFigures[1].inputNames[0] << std::endl;
+        double radius;
+        std::cin >> radius;
+        myFigure = std::make_unique<Circle>(radius);
+    }
+    else if(figureName == "triangle")
+    {
+        double a;
+        double b;
+        double c;
+
+        std::cout << "Enter length of " << possibleFigures[2].inputNames[0] << std::endl;
+        std::cin >> a;
+        std::cout << "Enter length of " << possibleFigures[2].inputNames[1] << std::endl;
+        std::cin >> b;
+        std::cout << "Enter length of " << possibleFigures[2].inputNames[2] << std::endl;
+        std::cin >> c;
+
+        myFigure = std::make_unique<Triangle>(a,b,c);
+    }
+    
+    if(myFigure != nullptr)
+    {
+        myFigure->print_circumference();
+        myFigure->print_area();
+    }
+    else
+    {
+        std::cout << "You have not chosen any possible figure" << std::endl;
+    }
     return 0;
 }
