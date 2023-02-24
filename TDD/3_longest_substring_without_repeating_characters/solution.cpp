3951d83c3511b685eddc7fab5c53174fba41a499
Katarzyna_Koczwara <katarzyna.b.koczwara@gmail.com>
2023-02-24 12:30:41 +0100

    Added aplication that returns length of the longest substring without repeating characters.

---
+++ TDD/3_longest_substring_without_repeating_characters/solution.cpp
@@ -0,0 +1,39 @@
+#include "solution.h"
+
+//Given a string s, find the length of the longest
+//substring without repeating characters.
+
+int Solution::lengthOfLongestSubstring(string s) 
+{
+    int result = 0;
+    int count = 0;
+    deque<char> letters;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(find(letters.begin(), letters.end(), s[i]) == letters.end())
+        {
+            letters.push_back(s[i]);
+            count += 1;
+        }
+        else
+        {
+            if(count > result)
+                result = count;
+
+            while(letters.front() != s[i])
+            {
+                letters.pop_front();
+                count --;
+            }
+            
+            letters.pop_front();
+            letters.push_back(s[i]);
+        }
+    }
+
+    if(count > result)
+        result = count;
+
+    return result;
+};
---
+++ TDD/3_longest_substring_without_repeating_characters/solution.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s);
+};
