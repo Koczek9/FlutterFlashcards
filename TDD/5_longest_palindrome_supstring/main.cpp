701a1f820ca4003afa0aab419908790b62a48eb9
Katarzyna_Koczwara <katarzyna.b.koczwara@gmail.com>
2023-02-24 12:41:48 +0100

    Added program that returns the longest palindrome in given string.

---
+++ TDD/5_longest_palindrome_supstring/main.cpp
@@ -0,0 +1,9 @@
+#include "solution.h"
+
+int main()
+
+{
+    Solution solution;
+    cout << "It is working" << endl;
+    return 0;
+}
---
+++ TDD/5_longest_palindrome_supstring/solution.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkIfPalindrome(string a);
+    string longestPalindrome(string s);
+};
