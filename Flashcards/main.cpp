8d7a18225faf1a5592ddba9e86a0419475cf336c
Katarzyna_Koczwara <katarzyna.b.koczwara@gmail.com>
2023-03-03 12:39:57 +0100

    Added imported from Kindle dictionary.csv.

--- Flashcards/main.cpp
+++ Flashcards/main.cpp
@@ -5,7 +5,7 @@
 int main() 
 {   
     std::vector<Flashcard> cards;
-    //cards = importFile("dictionary.csv");
+    //cards = importFile("../dictionary.csv");
     //writeXML(DATA_BASE_FILE_NAME, cards);
     cards = readFile(DATA_BASE_FILE_NAME);
     shuffle(cards);
